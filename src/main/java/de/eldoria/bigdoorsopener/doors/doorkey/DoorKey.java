48b8fc68eb91343d3fa5d97df54229d366aa584d
RainbowDashLabs <mail@code-fox.de>
2020-07-03 11:23:39 +0200

    add DoorKey interface for modular key creation

---
+++ src/main/java/de/eldoria/bigdoorsopener/doors/doorkey/DoorKey.java
@@ -0,0 +1,21 @@
+package de.eldoria.bigdoorsopener.doors.doorkey;
+
+import de.eldoria.bigdoorsopener.doors.ODoor;
+import org.bukkit.World;
+import org.bukkit.configuration.serialization.ConfigurationSerializable;
+import org.bukkit.entity.Player;
+
+public interface DoorKey extends ConfigurationSerializable {
+    /**
+     * Indicates if the key would open the door under the current circumstances.
+     *
+     * @param player player which should be checked.
+     * @param world world of the door
+     * @param door door data
+     * @param currentState the current state of the door.
+     * @return true if the key settings are matched.
+     */
+    boolean isOpen(Player player, World world, ODoor door, boolean currentState);
+
+    static void deserialize();
+}
