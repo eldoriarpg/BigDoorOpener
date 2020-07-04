a52056f28fda3742acd981a673a622f00d575a4f
RainbowDashLabs <mail@code-fox.de>
2020-07-05 00:56:31 +0200

    replace HeatCache with guava cache

--- src/main/java/de/eldoria/bigdoorsopener/util/CachingJSEngine.java
+++ src/main/java/de/eldoria/bigdoorsopener/util/CachingJSEngine.java
@@ -1,30 +1,34 @@
 package de.eldoria.bigdoorsopener.util;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 import de.eldoria.bigdoorsopener.BigDoorsOpener;
 
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 
 public class CachingJSEngine {
     private final ScriptEngine engine;
-    private final HeatCache<String, Object> cache;
+    private final Cache<String, Object> cache;
 
     public CachingJSEngine(int cacheSize) {
         engine = new ScriptEngineManager().getEngineByName("nashorn");
-        cache = new HeatCache<>(cacheSize);
+        cache = CacheBuilder.newBuilder().expireAfterAccess(24, TimeUnit.MINUTES).maximumSize(500).build();
     }
 
     @SuppressWarnings("unchecked")
     public <T> T eval(String string, T defaultValue) {
         try {
-            Object t = cache.computeIfAbsent(string, (k) -> {
+            Object t = cache.get(string, () -> {
                 try {
                     return engine.eval(string);
                 } catch (ScriptException e) {
                     BigDoorsOpener.logger().log(Level.WARNING,
-                            "An error occurred while evaluating " + string, e);
+                            "An error occurred while evaluating \"" + string + "\"", e);
                     return null;
                 }
             });
@@ -33,6 +37,10 @@ public class CachingJSEngine {
         } catch (ClassCastException e) {
             BigDoorsOpener.logger().log(Level.WARNING,
                     "Could not map result.", e);
+            return defaultValue;
+        } catch (ExecutionException e) {
+            BigDoorsOpener.logger().log(Level.WARNING,
+                    "Could not compute cache value.", e);
             return defaultValue;
         }
     }
