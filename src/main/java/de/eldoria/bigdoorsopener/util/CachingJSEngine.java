d05fb4eaf8908550fd6b44c382bee0b356c454b2
RainbowDashLabs <mail@code-fox.de>
2020-07-04 11:35:08 +0200

    add some illegal js shit.

---
+++ src/main/java/de/eldoria/bigdoorsopener/util/CachingJSEngine.java
@@ -0,0 +1,39 @@
+package de.eldoria.bigdoorsopener.util;
+
+import de.eldoria.bigdoorsopener.BigDoorsOpener;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+import java.util.logging.Level;
+
+public class CachingJSEngine<T> {
+    private final ScriptEngine engine;
+    private final HeatCache<String, Object> cache;
+
+    public CachingJSEngine() {
+        engine = new ScriptEngineManager().getEngineByName("nashorn");
+        cache = new HeatCache<>(200);
+    }
+
+    @SuppressWarnings("unchecked")
+    public T eval(String string, T defaultValue) {
+        try {
+            Object t = cache.computeIfAbsent(string, (k) -> {
+                try {
+                    return engine.eval(string);
+                } catch (ScriptException e) {
+                    BigDoorsOpener.logger().log(Level.WARNING,
+                            "An error occurred while evaluating " + string, e);
+                    return null;
+                }
+            });
+            if (t == null) return defaultValue;
+            return (T) t;
+        } catch (ClassCastException e) {
+            BigDoorsOpener.logger().log(Level.WARNING,
+                    "Could not map result.", e);
+            return defaultValue;
+        }
+    }
+}
