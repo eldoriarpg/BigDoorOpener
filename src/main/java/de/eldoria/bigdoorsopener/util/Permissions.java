d4c3fd245f93b02c6f60333b72898eb36ebcc7a8
RainbowDashLabs <mail@code-fox.de>
2020-07-06 01:01:14 +0200

    add new bypass permission and adjust reload permission

--- src/main/java/de/eldoria/bigdoorsopener/util/Permissions.java
+++ src/main/java/de/eldoria/bigdoorsopener/util/Permissions.java
@@ -2,5 +2,6 @@ package de.eldoria.bigdoorsopener.util;
 
 public class Permissions {
     public static final String USE = "bdo.use";
-    public static final String RELOAD = "bdo.use";
+    public static final String RELOAD = "bdo.reload";
+    public static final String ACCESS_ALL = "bdo.accessAll";
 }
