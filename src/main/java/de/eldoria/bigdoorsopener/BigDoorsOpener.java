c342ee20bcb8715cd8b8976dc8932dfa3c13c7b4
RainbowDashLabs <mail@code-fox.de>
2020-07-06 00:57:40 +0200

    tweake startup process

--- src/main/java/de/eldoria/bigdoorsopener/BigDoorsOpener.java
+++ src/main/java/de/eldoria/bigdoorsopener/BigDoorsOpener.java
@@ -1,14 +1,18 @@
 package de.eldoria.bigdoorsopener;
 
+import com.sk89q.worldguard.WorldGuard;
+import com.sk89q.worldguard.protection.regions.RegionContainer;
 import de.eldoria.bigdoorsopener.commands.BigDoorsOpenerCommand;
 import de.eldoria.bigdoorsopener.config.Config;
 import de.eldoria.bigdoorsopener.config.TimedDoor;
-import de.eldoria.bigdoorsopener.listener.TimeSkipListener;
-import de.eldoria.bigdoorsopener.scheduler.DoorApproachScheduler;
-import de.eldoria.bigdoorsopener.scheduler.TimedDoorScheduler;
+import de.eldoria.bigdoorsopener.doors.ConditionalDoor;
+import de.eldoria.bigdoorsopener.listener.WeatherListener;
+import de.eldoria.bigdoorsopener.listener.registration.RegisterInteraction;
+import de.eldoria.bigdoorsopener.scheduler.DoorChecker;
 import de.eldoria.eldoutilities.localization.Localizer;
 import de.eldoria.eldoutilities.messages.MessageSender;
 import de.eldoria.eldoutilities.updater.UpdateChecker;
+import lombok.Getter;
 import nl.pim16aap2.bigDoors.BigDoors;
 import nl.pim16aap2.bigDoors.Commander;
 import org.bstats.bukkit.Metrics;
@@ -35,10 +39,18 @@ public class BigDoorsOpener extends JavaPlugin {
     private boolean initialized;
     private Localizer localizer;
 
+    // External instances.
     private BigDoors doors;
+    @Getter
+    private static RegionContainer regionContainer = null;
     private Commander commander;
 
-    private TimeSkipListener timeSkipListener;
+    // scheduler
+    private DoorChecker doorChecker;
+
+    // listener
+    private WeatherListener weatherListener;
+    private RegisterInteraction registerInteraction;
 
     @Override
     public void onDisable() {
@@ -50,39 +62,78 @@ public class BigDoorsOpener extends JavaPlugin {
         PluginManager pm = Bukkit.getPluginManager();
         if (!initialized) {
             logger = this.getLogger();
-            UpdateChecker.performAndNotifyUpdateCheck(this, 80805, true);
-            ConfigurationSerialization.registerClass(TimedDoor.class, "timedDoor");
-            Plugin bigDoorsPlugin = pm.getPlugin("BigDoors");
-            doors = (BigDoors) bigDoorsPlugin;
-            commander = doors.getCommander();
+
+            buildSerializer();
+
+            // Load external resources before world guard.
+            loadExternalSources();
+
+            // create config
             config = new Config(this);
+
+
+            // Check for updates
+            if (config.isCheckUpdates()) {
+                UpdateChecker.performAndNotifyUpdateCheck(this, 80805, true);
+            }
+
+
             localizer = new Localizer(this, config.getLanguage(), "messages",
                     "messages", Locale.US, "de_DE", "en_US");
+
+            //enable metrics
             if (config.isEnableMetrics()) {
                 enableMetrics();
             }
+
+            registerListener();
+
+
             MessageSender.create(this, "§6[BDO] ", '2', 'c');
+
         }
 
-        TimedDoorScheduler timedDoorScheduler = new TimedDoorScheduler(doors, config, localizer);
+        doorChecker = new DoorChecker(config, doors, localizer);
 
         if (!initialized) {
-            timeSkipListener = new TimeSkipListener(timedDoorScheduler);
-            pm.registerEvents(timeSkipListener, this);
             getCommand("bigdoorsopener")
-                    .setExecutor(new BigDoorsOpenerCommand(this, commander, config, localizer, timedDoorScheduler));
+                    .setExecutor(new BigDoorsOpenerCommand(this, commander, config, localizer, doorChecker, registerInteraction));
         }
 
         if (initialized) {
-            timeSkipListener.reload(timedDoorScheduler);
             localizer.setLocale(config.getLanguage());
             scheduler.cancelTasks(this);
         }
 
-        scheduler.scheduleSyncRepeatingTask(this, new DoorApproachScheduler(config, doors, localizer), 100, config.getApproachRefreshRate());
-        scheduler.scheduleSyncRepeatingTask(this, timedDoorScheduler, 100, config.getTimedRefreshRate());
+        scheduler.scheduleSyncRepeatingTask(this, doorChecker, 100, 1);
 
         initialized = true;
+    }
+
+    private void registerListener() {
+        weatherListener = new WeatherListener();
+        Bukkit.getPluginManager().registerEvents(weatherListener, this);
+        registerInteraction = new RegisterInteraction();
+        Bukkit.getPluginManager().registerEvents(registerInteraction, this);
+    }
+
+    private void loadExternalSources() {
+        PluginManager pm = Bukkit.getPluginManager();
+
+        Plugin bigDoorsPlugin = pm.getPlugin("BigDoors");
+        doors = (BigDoors) bigDoorsPlugin;
+        commander = doors.getCommander();
+
+        // check if world guard is loaded
+        if (Bukkit.getPluginManager().isPluginEnabled("WorldGuard")) {
+            regionContainer = WorldGuard.getInstance().getPlatform().getRegionContainer();
+        }
+    }
+
+    private void buildSerializer() {
+        ConfigurationSerialization.registerClass(TimedDoor.class, "timedDoor");
+        ConfigurationSerialization.registerClass(ConditionalDoor.class, "conditionalDoor");
+
     }
 
 
