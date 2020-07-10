53e16f917941cbf875b7156e19c8b3cfec675847
RainbowDashLabs <mail@code-fox.de>
2020-07-10 16:40:11 +0200

    improve permissions

--- src/main/java/de/eldoria/bigdoorsopener/util/Permissions.java
+++ src/main/java/de/eldoria/bigdoorsopener/util/Permissions.java
@@ -1,7 +1,30 @@
 package de.eldoria.bigdoorsopener.util;
 
 public class Permissions {
-    public static final String USE = "bdo.use";
-    public static final String RELOAD = "bdo.reload";
+    // permission to use all commands except reload
+    public static final String USE = "bdo.command.use";
+    // permission to reload the plugin
+    public static final String RELOAD = "bdo.command.reload";
+    // permission to use a custom js evaluator
+    public static final String CUSTOM_EVALUATOR = "bdo.customEvaluator";
+    // permission to access door not owned by the player
     public static final String ACCESS_ALL = "bdo.accessAll";
+    // permission to access the item block condition
+    public static final String ITEM_BLOCK_CONDITION = "bdo.condition.item.block";
+    // permission to access the item block condition
+    public static final String ITEM_CLICK_CONDITION = "bdo.condition.item.click";
+    // permission to access the item holding condition
+    public static final String ITEM_HOLDING_CONDITION = "bdo.condition.item.";
+    // permission to access the item owning condition
+    public static final String ITEM_OWNING_CONDITION = "bdo.condition.item.";
+    // permission to access the proximity condition
+    public static final String PROXIMITY_CONDITION = "bdo.condition.location.proximity";
+    // permission to access the region condition
+    public static final String REGION_CONDITION = "bdo.condition.location.region";
+    // permission to access the permission condition
+    public static final String PERMISSION_CONDITION = "bdo.condition.permission";
+    // permission to access the time condition
+    public static final String TIME_CONDITION = "bdo.condition.time";
+    // permission to access the weather condition
+    public static final String WEATHER_CONDITION = "bdo.condition.weather";
 }
