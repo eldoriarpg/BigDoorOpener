856f134308985dfd4ee8a4a3e21117c7cd7c03cf
RainbowDashLabs <mail@code-fox.de>
2020-07-11 03:01:58 +0200

    remove deprecation warning for nashorn

--- src/main/java/de/eldoria/bigdoorsopener/util/CachingJSEngine.java
+++ src/main/java/de/eldoria/bigdoorsopener/util/CachingJSEngine.java
@@ -3,9 +3,9 @@ package de.eldoria.bigdoorsopener.util;
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
 import de.eldoria.bigdoorsopener.BigDoorsOpener;
+import jdk.nashorn.api.scripting.NashornScriptEngineFactory;
 
 import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
@@ -16,7 +16,8 @@ public class CachingJSEngine {
     private final Cache<String, Object> cache;
 
     public CachingJSEngine(int cacheSize) {
-        engine = new ScriptEngineManager().getEngineByName("nashorn");
+        NashornScriptEngineFactory factory = new NashornScriptEngineFactory();
+        engine = factory.getScriptEngine("--no-deprecation-warning");;
         cache = CacheBuilder.newBuilder().expireAfterAccess(24, TimeUnit.MINUTES).maximumSize(500).build();
     }
 
