e06b1f478e9135ae0d45ad23d6c7d2e1677a39da
RainbowDashLabs <mail@code-fox.de>
2020-07-11 03:04:28 +0200

    Add item condition listener

---
+++ src/main/java/de/eldoria/bigdoorsopener/listener/ItemConditionListener.java
@@ -0,0 +1,26 @@
+package de.eldoria.bigdoorsopener.listener;
+
+import de.eldoria.bigdoorsopener.config.Config;
+import de.eldoria.bigdoorsopener.doors.conditions.item.interacting.ItemInteraction;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+/**
+ * This listener controls when a player tries to open a door with a item.
+ */
+public class ItemConditionListener implements Listener {
+    private final Config config;
+
+    public ItemConditionListener(Config config) {
+        this.config = config;
+    }
+
+    public void onItemInteract(PlayerInteractEvent event) {
+        config.getDoors().values().forEach(d -> {
+            if (d.getConditionChain().getItem() == null) return;
+            if (d.getConditionChain().getItem() instanceof ItemInteraction) {
+                ((ItemInteraction) d.getConditionChain().getItem()).clicked(event);
+            }
+        });
+    }
+}
