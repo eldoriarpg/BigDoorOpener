67f2f3cc3b6878d582c541c723f06a30f69af7fe
RainbowDashLabs <mail@code-fox.de>
2020-09-18 14:28:41 +0200

    remove unused classes

--- src/main/java/de/eldoria/bigdoorsopener/doors/conditions/ConditionCreationException.java
+++
@@ -1,7 +0,0 @@
-package de.eldoria.bigdoorsopener.doors.conditions;
-
-public class ConditionCreationException extends RuntimeException {
-    public ConditionCreationException(String localeCode) {
-        super(localeCode);
-    }
-}
--- src/main/java/de/eldoria/bigdoorsopener/doors/conditions/CreationNotImplementedException.java
+++
@@ -1,7 +0,0 @@
-package de.eldoria.bigdoorsopener.doors.conditions;
-
-public class CreationNotImplementedException extends RuntimeException {
-    public CreationNotImplementedException() {
-        super("No creating routine is implemented, but a creation was requested.");
-    }
-}
