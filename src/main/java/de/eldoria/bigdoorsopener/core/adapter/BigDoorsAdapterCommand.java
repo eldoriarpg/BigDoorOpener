f4ac902f6a2ed288d93d8efcdac6d583512d741e
RainbowDashLabs <mail@code-fox.de>
2020-09-20 23:54:28 +0200

    remove not required code

--- src/main/java/de/eldoria/bigdoorsopener/core/adapter/BigDoorsAdapterCommand.java
+++ src/main/java/de/eldoria/bigdoorsopener/core/adapter/BigDoorsAdapterCommand.java
@@ -2,13 +2,11 @@ package de.eldoria.bigdoorsopener.core.adapter;
 
 import de.eldoria.bigdoorsopener.config.Config;
 import de.eldoria.bigdoorsopener.core.BigDoorsOpener;
-import de.eldoria.bigdoorsopener.core.events.DoorRegisteredEvent;
 import de.eldoria.bigdoorsopener.door.ConditionalDoor;
 import de.eldoria.bigdoorsopener.util.C;
 import de.eldoria.bigdoorsopener.util.Permissions;
 import de.eldoria.eldoutilities.container.Pair;
 import de.eldoria.eldoutilities.localization.Localizer;
-import de.eldoria.eldoutilities.localization.Replacement;
 import de.eldoria.eldoutilities.messages.MessageSender;
 import de.eldoria.eldoutilities.simplecommands.EldoCommand;
 import de.eldoria.eldoutilities.utils.ArrayUtil;
@@ -50,102 +48,6 @@ public abstract class BigDoorsAdapterCommand extends EldoCommand {
         messageSender = BigDoorsOpener.getPluginMessageSender();
     }
 
-    /**
-     * Set the state of the door.
-     *
-     * @param open true if the door should be open.
-     * @param door door to set the state
-     * @return true if the door state was changed succesfully which is indicated by {@link BigDoors#toggleDoor(long)}
-     */
-    protected boolean setDoorState(boolean open, ConditionalDoor door) {
-        if (commander.isDoorBusy(door.getDoorUID())) {
-            return false;
-        }
-        if (isOpen(door) == open) return false;
-        return bigDoors.toggleDoor(door.getDoorUID());
-    }
-
-    /**
-     * Check if a door is currently considered as open.
-     * Will use the {@link ConditionalDoor#openInverted(boolean)} method.
-     *
-     * @param door door to check.
-     * @return true when the door is open.
-     */
-    protected boolean isOpen(ConditionalDoor door) {
-        return door.openInverted(bigDoors.isOpen(door.getDoorUID()));
-    }
-
-    /**
-     * Checks if the world of a door exists and is present in the plugin.
-     *
-     * @param door door to check
-     * @return true if the door exits
-     */
-    protected boolean doorExists(ConditionalDoor door) {
-        World world = server.getWorld(door.getWorld());
-        if (world == null) {
-            BigDoorsOpener.logger().info(localizer.getMessage("error.worldIsNull",
-                    Replacement.create("DOOR_NAME", door.getDoorUID())));
-            return false;
-        }
-
-        // Make sure that this door still exists on the doors plugin.
-        return getDoor(door.getDoorUID()) != null;
-    }
-
-    /**
-     * Checks if a door is available.
-     * A door is considered available if its not open and not busy
-     *
-     * @param door door to check
-     * @return true if the door is available
-     */
-    protected boolean isAvailableToOpen(ConditionalDoor door) {
-        return !isOpen(door) && !isBusy(door) && isDoorLoaded(getDoor(door.getDoorUID()));
-    }
-
-    /**
-     * Checks if the chunks of a door are loaded
-     *
-     * @param door door to check
-     * @return true if chunks around the door are loaded
-     */
-    protected boolean isDoorLoaded(Door door) {
-        return bigDoors.areChunksLoadedForDoor(door);
-    }
-
-    /**
-     * Checks if a door is busy. A door is busy if its closing or opening at the moment.
-     *
-     * @param door door to check
-     * @return true if the door is busy
-     */
-    protected boolean isBusy(ConditionalDoor door) {
-        return commander.isDoorBusy(door.getDoorUID());
-    }
-
-    /**
-     * Get the door with the specified id if the player is the owner of the door.
-     *
-     * @param uid uid of the door.
-     * @return door with id if exitsts
-     */
-    @Nullable
-    protected Door getDoor(Player player, long uid) {
-        return commander.getDoor(player.getUniqueId(), uid);
-    }
-
-    /**
-     * Get the door with the specified id.
-     *
-     * @param doorId uid of the door.
-     * @return door with id if exitsts
-     */
-    @Nullable
-    protected Door getDoor(long doorId) {
-        return commander.getDoor(null, doorId);
-    }
 
     /**
      * Get the door with a specific id.
@@ -200,10 +102,6 @@ public abstract class BigDoorsAdapterCommand extends EldoCommand {
         return commander.getDoors();
     }
 
-    protected Localizer getLocalizer() {
-        return localizer;
-    }
-
     /**
      * Tries to find a door.
      * Will search for a door of the player.
@@ -223,6 +121,7 @@ public abstract class BigDoorsAdapterCommand extends EldoCommand {
                 messageSender.sendError(null, localizer.getMessage("error.doorNotFound"));
                 return null;
             }
+            door.setPermission(0);
             return door;
         }
 
@@ -230,14 +129,23 @@ public abstract class BigDoorsAdapterCommand extends EldoCommand {
         List<Door> doors = getDoors(player, doorUID);
 
         if (doors.isEmpty()) {
+            // try to retrieve door by player
+            Door door = getDoor(player, doorUID);
+
+            if (door != null) {
+                return door;
+            }
+
             // door is null. check if door exists anyway
-            Door door = getDoor(doorUID);
+            door = getDoor(doorUID);
             if (door == null) {
                 messageSender.sendError(player, localizer.getMessage("error.doorNotFound"));
                 return null;
             }
+
             // when the door exists and the player has access to all doors return it.
             if (player.hasPermission(Permissions.ACCESS_ALL)) {
+                door.setPermission(0);
                 return door;
             } else {
                 messageSender.sendError(player, localizer.getMessage("error.notYourDoor"));
@@ -279,11 +187,8 @@ public abstract class BigDoorsAdapterCommand extends EldoCommand {
                 (max.getY() + min.getY()) / 2,
                 (max.getZ() + min.getZ()) / 2);
 
-        ConditionalDoor conditionalDoor = config.computeDoorIfAbsent(door.getDoorUID(),
+        return config.computeDoorIfAbsent(door.getDoorUID(),
                 doorId -> new ConditionalDoor(doorId, world.getName(), vector));
-
-        Bukkit.getPluginManager().callEvent(new DoorRegisteredEvent(conditionalDoor));
-        return conditionalDoor;
     }
 
     /**
