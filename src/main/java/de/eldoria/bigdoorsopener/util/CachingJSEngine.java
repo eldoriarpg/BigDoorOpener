dd840257eafb608c4995c3bba11c7cc0caa1b899
mail@chojo.de <mail@chojo.de>
2021-03-13 12:20:59 +0100

    minor changes

--- src/main/java/de/eldoria/bigdoorsopener/util/CachingJSEngine.java
+++ src/main/java/de/eldoria/bigdoorsopener/util/CachingJSEngine.java
@@ -18,77 +18,86 @@ import java.util.logging.Level;
  * which it should be anyway.
  */
 public class CachingJSEngine {
-	private final Cache<String, Object> cache;
-	private ScriptEngine engine;
+    private final Cache<String, Object> cache;
+    private ScriptEngine engine;
 
-	public CachingJSEngine(int cacheSize) {
-		try {
-			String[] versionString = System.getProperty("java.specification.version").split("\\.");
-			String version = versionString[versionString.length - 1];
-			if (Integer.parseInt(version) < 11) {
-				this.engine = (new NashornScriptEngineFactory()).getScriptEngine();
-			} else if (Integer.parseInt(version) < 15) {
-				this.engine = (new NashornScriptEngineFactory()).getScriptEngine("--no-deprecation-warning");
-			} else {
+    public CachingJSEngine(int cacheSize) {
+        try {
+            BigDoorsOpener.logger().info("Java version: " + System.getProperty("java.specification.version"));
+            String[] versionString = System.getProperty("java.specification.version").split("\\.");
+            String version = versionString[versionString.length - 1];
+            int verInt = Integer.parseInt(version);
+            BigDoorsOpener.logger().info("Detected version: " + verInt);
+            if (verInt < 11) {
+                BigDoorsOpener.logger().info("Detected legacy java version below 11.");
+                this.engine = new NashornScriptEngineFactory().getScriptEngine();
+            } else if (verInt < 15) {
+                BigDoorsOpener.logger().info("Java 11 or newer detected.");
+                this.engine = new NashornScriptEngineFactory().getScriptEngine("--no-deprecation-warning");
+            } else {
                 this.engine = new ScriptEngineManager(null).getEngineByName("JavaScript");
             }
-			this.engine.eval("print('[BigDoorsOpener] nashorn script engine started.')");
-		} catch (ScriptException e) {
-			BigDoorsOpener.logger().info("No nashorn script engine found. Trying to use JavaScript fallback.");
-			engine = new ScriptEngineManager(null).getEngineByName("JavaScript");
-			try {
-				engine.eval("print('[BigDoorsOpener] JavaScript script engine started.')");
-			} catch (ScriptException ex) {
-				BigDoorsOpener.logger().warning("Could not start script engine. Custom evaluator will not work.");
-			}
-		}
+            this.engine.eval("print('[BigDoorsOpener] nashorn script engine started.')");
+        } catch (ScriptException e) {
+            BigDoorsOpener.logger().log(Level.WARNING, "No nashorn script engine found. Trying to use JavaScript fallback.", e);
+            engine = new ScriptEngineManager(null).getEngineByName("JavaScript");
+            try {
+                engine.eval("print('[BigDoorsOpener] JavaScript script engine started.')");
+            } catch (ScriptException ex) {
+                BigDoorsOpener.logger().log(Level.WARNING, "Could not start script engine. Custom evaluator will not work.", e);
+            }
+        } catch (RuntimeException e) {
+            BigDoorsOpener.logger().log(Level.WARNING, "Could not start script engine. Custom evaluator will not work.", e);
 
-		cache = CacheBuilder.newBuilder().expireAfterAccess(24, TimeUnit.MINUTES).maximumSize(500).build();
-	}
+        }
 
-	/**
-	 * Evaluates a string with a js engine.
-	 *
-	 * @param string       string to evaluate
-	 * @param defaultValue default value which should be returned if anything goes wrong.
-	 * @param <T>          type which should be returned
-	 *
-	 * @return evaluated value or default value
-	 */
-	@SuppressWarnings("unchecked")
-	public <T> T eval(String string, T defaultValue) {
-		try {
-			return evalUnsafe(string, defaultValue);
-		} catch (ScriptException e) {
-			BigDoorsOpener.logger().log(Level.WARNING,
-					"An error occurred while evaluating \"" + string + "\"", e);
-			return defaultValue;
-		} catch (ClassCastException e) {
-			BigDoorsOpener.logger().log(Level.WARNING,
-					"Could not map result.", e);
-			return defaultValue;
-		} catch (ExecutionException e) {
-			BigDoorsOpener.logger().log(Level.WARNING,
-					"Could not compute cache value for " + string + ".", e);
-			return defaultValue;
-		}
-	}
 
-	/**
-	 * @param string       string to evaluate
-	 * @param defaultValue default value which should be returned if anything goes wrong.
-	 * @param <T>          type which should be returned
-	 *
-	 * @return evaluated value or default value if evaluated value is null.
-	 *
-	 * @throws ExecutionException if a exception occured while creating the cached value
-	 * @throws ScriptException    if a exception orccured while executing the js script
-	 * @throws ClassCastException if the received object cant be cast on the requested type
-	 */
-	@SuppressWarnings("unchecked")
-	public <T> T evalUnsafe(String string, T defaultValue) throws ExecutionException, ScriptException, ClassCastException {
-		Object t = cache.get(string, () -> engine.eval(string));
-		if (t == null) return defaultValue;
-		return (T) t;
-	}
+        cache = CacheBuilder.newBuilder().expireAfterAccess(24, TimeUnit.MINUTES).maximumSize(500).build();
+    }
+
+    /**
+     * Evaluates a string with a js engine.
+     *
+     * @param string       string to evaluate
+     * @param defaultValue default value which should be returned if anything goes wrong.
+     * @param <T>          type which should be returned
+     *
+     * @return evaluated value or default value
+     */
+    @SuppressWarnings("unchecked")
+    public <T> T eval(String string, T defaultValue) {
+        try {
+            return evalUnsafe(string, defaultValue);
+        } catch (ScriptException e) {
+            BigDoorsOpener.logger().log(Level.WARNING,
+                    "An error occurred while evaluating \"" + string + "\"", e);
+            return defaultValue;
+        } catch (ClassCastException e) {
+            BigDoorsOpener.logger().log(Level.WARNING,
+                    "Could not map result.", e);
+            return defaultValue;
+        } catch (ExecutionException e) {
+            BigDoorsOpener.logger().log(Level.WARNING,
+                    "Could not compute cache value for " + string + ".", e);
+            return defaultValue;
+        }
+    }
+
+    /**
+     * @param string       string to evaluate
+     * @param defaultValue default value which should be returned if anything goes wrong.
+     * @param <T>          type which should be returned
+     *
+     * @return evaluated value or default value if evaluated value is null.
+     *
+     * @throws ExecutionException if a exception occured while creating the cached value
+     * @throws ScriptException    if a exception orccured while executing the js script
+     * @throws ClassCastException if the received object cant be cast on the requested type
+     */
+    @SuppressWarnings("unchecked")
+    public <T> T evalUnsafe(String string, T defaultValue) throws ExecutionException, ScriptException, ClassCastException {
+        Object t = cache.get(string, () -> engine.eval(string));
+        if (t == null) return defaultValue;
+        return (T) t;
+    }
 }
