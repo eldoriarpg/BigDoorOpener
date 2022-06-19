8a627c92cf341530f42d6c2ad24b68bb8ce5971b
Lilly Tempest <46890129+RainbowDashLabs@users.noreply.github.com>
2022-06-19 14:38:49 +0200

    Release 2.4.6 (#84)
    * Configure Renovate (#62)
    
    * Add renovate.json
    
    * Update renovate.json
    
    Co-authored-by: Lilly Tempest <46890129+RainbowDashLabs@users.noreply.github.com>
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    * Update dependency de.eldoria:eldo-util to v1.13.5 (#67)
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    * Update dependency nl.pim16aap2:BigDoors to v0.1.8.32 (#65)
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    * Update plugin com.github.johnrengelman.shadow to v7.1.2 (#66)
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    * Update dependency gradle to v7.4.2 (#69)
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    * Update dependency io.lumine.xikage:MythicMobs to v4.12.0 (#70)
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    * Update dependency net.kyori:adventure-api to v4.10.1 (#72)
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    * Update dependency org.junit.jupiter:junit-jupiter-api to v5.8.2 (#73)
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    * Update actions/checkout action to v3 (#74)
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    * Update actions/setup-java action to v3 (#75)
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    * Update dependency org.jetbrains:annotations to v23 (#76)
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    * Update dependency net.kyori:adventure-platform-bukkit to v4.1.0 (#77)
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    * Update dependency me.clip:placeholderapi to v2.11.1 (#71)
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    * Update dependency net.kyori:adventure-api to v4.11.0 (#81)
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    * Update dependency net.kyori:adventure-platform-bukkit to v4.1.1 (#83)
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    * Update to 1.19 and add auto close event listener
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    Co-authored-by: Renovate Bot <bot@renovateapp.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("org.cadixdev.licenser") version "0.6.1"
-    id("com.github.johnrengelman.shadow") version "7.1.0"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
     java
     `maven-publish`
     `java-library`
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "de.eldoria"
-version = "2.4.5"
+version = "2.4.6"
 var mainPackage = "bigdoorsopener"
 val shadebade = group as String? + "." + mainPackage + "."
 val name = "BigDoorsOpener"
@@ -22,18 +22,17 @@ repositories {
 }
 
 dependencies {
-    implementation("de.eldoria", "eldo-util", "1.12.9-DEV")
-    implementation("net.kyori", "adventure-api", "4.8.1")
-    implementation("net.kyori", "adventure-platform-bukkit", "4.0.0-SNAPSHOT")
+    implementation("de.eldoria", "eldo-util", "1.13.5")
+    implementation("net.kyori", "adventure-platform-bukkit", "4.1.1")
     compileOnly("org.spigotmc", "spigot-api", "1.13.2-R0.1-SNAPSHOT")
-    compileOnly("org.jetbrains", "annotations", "20.1.0")
+    compileOnly("org.jetbrains", "annotations", "23.0.0")
     compileOnly("com.sk89q.worldguard", "worldguard-bukkit", "7.0.5-SNAPSHOT") {
         exclude("org.spigotmc", "spigot-api")
     }
-    compileOnly("me.clip", "placeholderapi", "2.10.6")
-    compileOnly("io.lumine.xikage", "MythicMobs", "4.9.1")
-    compileOnly("nl.pim16aap2", "BigDoors", "0.1.8.28")
-    testImplementation("org.junit.jupiter", "junit-jupiter-api", "5.5.2")
+    compileOnly("me.clip", "placeholderapi", "2.11.1")
+    compileOnly("io.lumine.xikage", "MythicMobs", "4.12.0")
+    compileOnly("nl.pim16aap2", "BigDoors", "0.1.8.38-SNAPSHOT")
+    testImplementation("org.junit.jupiter", "junit-jupiter-api", "5.8.2")
 }
 
 license {
